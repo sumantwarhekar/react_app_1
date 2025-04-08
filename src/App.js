ffe59b6d9b0035bba0ad67381b29ffe2dcd88477
Sumant Warhekar <sumantwarhekar@gmail.com>
2025-04-07 20:14:09 -0400

    playing with State in React

--- src/App.js
+++ src/App.js
@@ -5,20 +5,21 @@ import './index.css'
 
 function App() {
 
-  const[value, setValue] = useState('');
+  const[count,setCount] = useState(0);
+  const[prevCount, setPrevCount] = useState(0);
 
-  const eventHandler = (e) => {
-    setValue(e.target.value);
-    console.log(e.target);
-  };
+  const handlelick = () => {
+    setCount((prev) => {
+      setPrevCount(prev);
+    })
+    setCount(count + 1);
+  }
 
   return(
     <>
-      <input 
-        value={value}
-        placeholder='Enter some text'
-        onChange={eventHandler}
-      />
+      <h3> Current Count : {count} </h3>
+      <h3> Previous Count : {prevCount} </h3>
+      <button onClick={handlelick}>Increment</button>
     </>
   );
 }
