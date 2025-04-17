205b2c495ab72e2231acf8e4d6fce617040ff2c8
Sumant Warhekar <sumantwarhekar@gmail.com>
2025-04-16 20:13:28 -0400

    Checking out the context topic in React

--- src/App.js
+++ src/App.js
@@ -1,66 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext, createContext } from 'react';
 import './index.css'
 
-const CountDown = ({hr,min,sec}) => {
-  const [over,setOver] = useState(false);
-  const [paused, setPaused] = useState(true);
-  const [[h,m,s], setTime] = useState([hr,min,sec]);
+const CountContext = new createContext();
 
-  const tick = () => {
-    if( paused || over){
-        return;
-    }
-    if(h===0 && m===0 && s===0){
-      setOver(true);
-    }
-    else if(m===0 && s===0){
-      setTime([h-1,59,59]);
-    }
-    else if(s===0){
-      setTime([h,m-1,59]);
-    }
-    else{
-      setTime([h,m,s-1]);
-    }
-  }
-  
-  const handleReset = () => {
-    setTime([hr,min,sec]);
-    setPaused(true);
-    setOver(false);
-  }
-  
-  const handlePause = () => {
-    setPaused(!paused);
-  }
-  
-  const fmt = (val) => {
-    return val.toString().padStart(2,'0');
-  }
+function CountProvider ({children}){
+  const[count,setCount] = useState(0);
 
-  useEffect (() => {
-    let ticker = setInterval(() => tick(),1000);
-    return () => {
-      clearInterval(ticker);
-    }
-  });
-
-  return (
-    <>
-    <h3 className='countdown'>{`${fmt(h)}:${fmt(m)}:${fmt(s)}`}</h3>
-    <button onClick={handlePause}>{paused?'Start':'Pause'}</button>
-    <button onClick={handleReset}>Reset</button>
-    </>
+  return(
+    <CountContext.Provider value={{count,setCount}}>
+      {children}
+    </CountContext.Provider>
   )
-};
+}
+
+function Count(){
+  const {count} = useContext(CountContext);
+  return <h3>{`Current Count: ${count}`}</h3>
+}
+
+function CountButton(){
+  const {setCount} = useContext(CountContext);
+  return(
+    <button onClick={() => setCount(count => count + 1)}>
+      Increment
+    </button>
+  )
+}
 
 function App() {
   return(
-    <div>
-      <CountDown hr={0} min={0} sec={10} />
-    </div>
+    <CountProvider>
+      <Count/>
+      <CountButton/>
+    </CountProvider>
   );
 }
 
